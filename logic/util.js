556d2a9e79c7415126460555872a7d26d75fc5ec
Soapey <granttravissoper@gmail.com>
2022-06-21 20:22:16 +1000

    -a

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,19 @@
 function Util() {
+    function calculateTotalDistributedPassengers(object) {
+        return  object.vipPassengersWithBusinessSeats + 
+                object.vipPassengersWithEconomySeats + 
+                object.regularPassengersWithBusinessSeats + 
+                object.regularPassengersWithEconomySeats;
+    }
 
+    function calculateTotalNumberOfPassengers(passengersArray) {
+        let totalPassengers = 0;
+        passengersArray.map(function(arrayCell) {totalPassengers += arrayCell});
+        return totalPassengers;
+    }
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 }
+
+module.exports = Util();
 
