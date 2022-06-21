dddcf7865ed4da4b476dff4d4a24eab741b77e0b
Soapey <granttravissoper@gmail.com>
2022-06-21 20:36:24 +1000

    -a

--- logic/price.js
+++ logic/price.js
@@ -1,9 +1,9 @@
 function Prices() {
     function calculateFinalPrice(basePrice, passengerVariation, flightVariation) {
         let finalPrice = basePrice;
-        finalPrice += (basePrice * passengerVariation);
-        finalPrice += (basePrice * flightVariation);
-        
+        finalPrice += (finalPrice * passengerVariation);
+        finalPrice += (finalPrice * flightVariation);
+
         return finalPrice.toFixed(2);
     }
 
